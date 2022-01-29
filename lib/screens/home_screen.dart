fecd4431865494f58095af4e1af52c9bccdb7a3d
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-01-28 19:05:11 -0600

    Detalles del HomeScreen - Tema global del AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Peliculas en cines'),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined))
+        ],
+      ),
+      body: Column(
+        children: const [
+          // TODO: CardSwiper
+          CardSwiper(),
+
+          // Listado horizontal de peliculas
+        ],
       ),
     );
   }
