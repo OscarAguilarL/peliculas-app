242d02878dc3051e0e93e324fa80e797e9068fd2
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-01 18:02:05 -0600

    Peticiones HTTP

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,14 +1,32 @@
 // ignore_for_file: unnecessary_this
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+  String _baseUrl = 'api.themoviedb.org';
+  String _apiKey = '26226bc0d351dd2e356a4f74d1f1d276';
+  String _language = 'es-ES';
+
   MoviesProvider() {
     print('Movies provider inicializado');
     this.getOnDisplayMovies();
   }
 
-  getOnDisplayMovies() {
-    print('getOnDisplayMovies');
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+
+    final response = await http.get(url);
+
+    if (response.statusCode != 200) return print('error');
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+
+    print(decodedData['results']);
   }
 }
